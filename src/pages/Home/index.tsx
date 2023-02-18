7e05d15e1d3af7937dbe71d501f20c37eb582fff
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-18 03:39:38 -0300

    feat(Adding app description and login button to HomePage): :sparkles:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,19 @@
 import logoHorizontal from 'assets/img/logo-horizontal.png'
+import { GoogleLogo } from 'phosphor-react'
+
 export function HomePage() {
   return (
-    <div className="text-text flex h-screen w-full items-center justify-center p-8">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-4 bg-home bg-cover bg-center bg-no-repeat p-8 text-chattou-text">
       <img src={logoHorizontal} alt="" />
+      <h1 className="text-center text-xl text-chattou-text md:text-2xl lg:text-4xl">
+        Discover a whole new world of connection with CHATTOU! Chat with people
+        from all over the world in real time, make new friends and share your
+        ideas and experiences. Join our global community right now.
+      </h1>
+      <button className="focus-default flex items-center justify-center gap-2 rounded-md bg-red-500 p-2 px-4 text-2xl outline-none transition-colors hover:bg-red-700 hover:text-chattou-textDark">
+        <GoogleLogo weight="fill" size="24" />
+        Login
+      </button>
     </div>
   )
 }
