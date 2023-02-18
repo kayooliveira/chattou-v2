26382d46aef5ee04b83a33488211a0d752ebb01d
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-18 14:19:26 -0300

    fix(Making the homepage have an absolute position ensuring that the background of the page extends across the entire screen instead of being cut off at 1280px on larger screens.): :art:

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,17 +3,20 @@ import { GoogleLogo } from 'phosphor-react'
 
 export function HomePage() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-4 bg-home bg-cover bg-center bg-no-repeat p-8 text-chattou-text">
-      <img src={logoHorizontal} alt="" />
-      <h1 className="text-center text-xl text-chattou-text md:text-2xl lg:text-4xl">
-        Discover a whole new world of connection with CHATTOU! Chat with people
-        from all over the world in real time, make new friends and share your
-        ideas and experiences. Join our global community right now.
-      </h1>
-      <button className="focus-default flex items-center justify-center gap-2 rounded-md bg-red-500 p-2 px-4 text-2xl outline-none transition-colors hover:bg-red-700 hover:text-chattou-textDark">
-        <GoogleLogo weight="fill" size="24" />
-        Login
-      </button>
+    <div className="absolute left-0 top-0 h-full w-screen bg-home bg-cover bg-center bg-no-repeat p-8">
+      <div className="mx-auto flex h-full max-w-screen-xl flex-col items-center justify-center gap-4">
+        <img src={logoHorizontal} alt="" />
+        <h1 className="text-center text-xl md:text-2xl lg:text-4xl">
+          Discover a whole new world of connection with CHATTOU!
+          <br /> Chat with people from all over the world in real time, make new
+          friends and share your ideas and experiences.
+          <br /> Join our global community right now.
+        </h1>
+        <button className="focus-default flex items-center justify-center gap-2 rounded-md bg-red-500 p-2 px-4 text-2xl outline-none transition-colors hover:bg-red-700 hover:text-chattou-textDark">
+          <GoogleLogo weight="fill" size="24" />
+          Login
+        </button>
+      </div>
     </div>
   )
 }
