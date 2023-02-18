691c37067094b7b51f96fb6db09b83d8656f85b3
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-18 15:29:26 -0300

    feat(Adding react-hot-toast library to project): :heavy_plus_sign:

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,18 @@
+import chattouIcon from 'assets/img/logotipo.png'
+import { Toaster } from 'react-hot-toast'
 import { RouterProvider } from 'react-router-dom'
 import { router } from 'router'
-
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <Toaster
+        toastOptions={{
+          duration: 5000,
+          position: 'top-center',
+          icon: <img src={chattouIcon} width="20" alt="" />
+        }}
+      />
+      <RouterProvider router={router} />
+    </>
+  )
 }
