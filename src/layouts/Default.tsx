2a7fc3e705f6473ab859ad8ee28f3c676657da85
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-05-08 00:37:07 -0300

    feat(Adding "buy me a coffee" button): :sparkles:

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -6,7 +6,20 @@ import { Loading } from './components/Loading'
 export function DefaultLayout() {
   const isAuthStateLoading = useAuthStore(state => state.isAuthStateLoading)
   return (
-    <div className="h-full max-h-full w-full max-w-screen-xl lg:mx-auto">
+    <div className="relative h-full max-h-full w-full max-w-screen-xl lg:mx-auto">
+      <div className="absolute right-6 bottom-6">
+        <a
+          href="https://www.buymeacoffee.com/kayooliveira"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            style={{ height: '36.3px', width: '131.285px' }}
+          />
+        </a>
+      </div>
       {isAuthStateLoading ? <Loading message="Autenticando..." /> : <Outlet />}
     </div>
   )
